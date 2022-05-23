0053a3f7b47ea3e625e27de9878b9e6d4a5649f8
CarlozzoGuido <carlozzo.guido@iisgalvanimi.edu.it>
2022-05-23 11:41:35 +0000

    V1

---
+++ script.js
@@ -0,0 +1,12 @@
+function buttonfuction0(url){
+    document.getElementById("myframe").src=url;
+}
+function buttonfuction1(url){
+    document.getElementById("myframe").src=url;
+}
+function buttonfuction2(url){
+    document.getElementById("myframe").src=url;
+}
+function buttonfuction3(url){
+    document.getElementById("myframe").src=url;
+}
